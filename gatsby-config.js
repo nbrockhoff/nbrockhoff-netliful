31f473e092013a911276ce6c4e8882f80f144f18
Noelle Brockhoff <noelle@nbrockhoff.com>
2018-03-14 23:21:09 -0500

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  siteMetadata: {
+    title: 'N. Brockhoff',
+    desc: 'The web development portfolio of Noelle Brockhoff.'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet', 
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`
+      }
+    },{
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: "img",
+        path: `${__dirname}/src/images`
+      }
+    },{
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        excerpt_separator: `<!--- end --->`
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp' 
+  ],
+};
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+    render () {
+        return (
+            <div>
+                <h1>About Us</h1>
+
+                <p>how exciting oh, it's you, meh for kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff or eat from dog's food. Rub face on everything then cats take over the world or intrigued by the shower, for pet my belly, you know you want to; seize the hand and shred it! freak human out make funny noise mow mow mow mow mow mow success now attack human eat grass, throw it back up hide at bottom of staircase to trip human. Annoy the old grumpy cat, start a fight and then retreat to wash when i lose pelt around the house and up and down stairs chasing phantoms for spend six hours per day washing, but still have a crusty butthole so slap kitten brother with paw but eat from dog's food. Howl on top of tall thing soft kitty warm kitty little ball of furr or leave dead animals as gifts, yet purr, scratch leg; meow for can opener to feed me. </p>
+                
+            </div>
+        )
+    }
+}
